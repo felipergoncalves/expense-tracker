bff8867524e03145eec268b0e039306809f1e619
Felipe <feferodrigues771@gmail.com>
2023-06-28 19:00:32 -0300

    InfoArea, updating the date on click

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {categories} from './data/categories'
 import {items} from './data/items'
 import {getCurrentMonth, filterListByMonth} from './helpers/dateFilter'
 import { TableArea } from './components/TableArea'
+import { InfoArea } from './components/InfoArea'
 
 const App = () => {
   const [list, setList] = useState(items);
@@ -16,13 +17,17 @@ const App = () => {
     setFilteredList( filterListByMonth(list, currentMonth) );
   }, [list, currentMonth]);
 
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
+
   return(
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        {/* Área de informações */}
+        <InfoArea currentMonth={currentMonth} onMonthChange={handleMonthChange}/>
 
         {/* Área de inserção */}
         
--- src/data/items.ts
+++ src/data/items.ts
@@ -7,5 +7,6 @@ export const items: Item[] = [
     { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4500 },
     { date: new Date(2023, 5, 20), category: 'food', title: 'McDonalds', value: 32.25},
     { date: new Date(2023, 5, 8), category: 'food', title: 'Burguer King', value: 25},
+    { date: new Date(2023, 5, 8), category: 'rent', title: 'Aluguel', value: 1200},
     { date: new Date(2023, 5, 15), category: 'salary', title: 'Salário ACME', value: 1000},
 ];
